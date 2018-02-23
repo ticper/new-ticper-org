93ef13c9caadeb2b7c147680623d9099652afa9e
Ryo Nakagome <nakagome@tento-net.com>
2018-02-23 17:13:31 +0900

    Configファイルを追加

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+  $db_host = 'db.ticper.com';
+  $db_user = 'ticper';
+  $db_pass = 'ticp-37648';
+  $db_name = 'ticper';
+
+  $db_link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+?>
